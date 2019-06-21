62348c3cb9a212550ada16ce52f0708e2c4b7da5
basilcea <ogbonna.basil3@gmmail.com>
2019-06-21 09:24:11 +0100

    create actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,7 @@
   Be sure to export each action type so you can pull it into your reducer
 
 */
+import axios from 'axios';
 
 export const FETCH = 'FETCH';
 export const SUCCESS = 'SUCCESS';
@@ -21,3 +22,60 @@ export const DELETE = 'DELETE';
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const success = value => {
+  return {
+    type: SUCCESS,
+    payload: value
+  }
+}
+export const failure = value => {
+  return {
+    type: FAILURE,
+    payload: value
+  }
+}
+
+export const getSmurfs = () => async dispatch =>{
+  dispatch({type: FETCH});
+  const AxiosData = await axios.get('http://localhost:3333/smurfs')
+  try{
+    dispatch(success(AxiosData.data.results))
+  }
+  catch(err){
+    dispatch(failure(AxiosData.statusText))
+  }
+}
+export const addSmurf = (data) => async dispatch => {
+  dispatch({type: CREATE}); 
+  try{
+    await axios.post('http://localhost:3333/smurfs' , data)
+    dispatch(getSmurfs())
+  }
+  catch(err){
+    dispatch(failure(err.message))
+  }
+}
+
+export const updateSmurfs = (id , data) => async dispatch => {
+  dispatch ({type: UPDATE});
+
+  try{
+    await axios.put(`http://localhost:3333/smurfs/${id}`, data)
+    dispatch(getSmurfs())
+  }
+  catch(err){
+    dispatch(failure(err.message))
+  }
+
+}
+export const deleteSmurf = (id) =>  async dispatch =>{
+  dispatch ({type: DELETE});
+  try{
+    await axios.delete(`http://localhost:3333/smurfs/${id}`)
+    dispatch(getSmurfs())
+  }
+  catch(err){
+    dispatch(failure(err.message))
+  }
+}
